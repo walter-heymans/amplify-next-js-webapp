782b05bd68596b2b9f129687db3bcc40031df073
Walter Heymans <walterh@wtax.co>
2026-03-31 07:58:44 +0200

    amplify: Override Lambda runtime to Node.js 22
    Amplify Gen 2 defaults to an older Node.js runtime. Override all
    generated Lambda functions to use nodejs22.x via CDK.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { Stack } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+});
+
+// Override all Lambda functions to use Node.js 22
+const dataStack = Stack.of(backend.data);
+dataStack.node.findAll().forEach((child) => {
+  if (child instanceof lambda.CfnFunction) {
+    child.runtime = 'nodejs22.x';
+  }
 });
